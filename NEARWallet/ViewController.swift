544fa793a10bd4ec299f955bc2665b1c4df2886d
Vladimir Grichina <vgrichina@gmail.com>
2020-05-22 15:34:06 -0700

    Initial Commit

---
+++ NEARWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NEARWallet
+//
+//  Created by Vladimir Grichina on 5/22/20.
+//  Copyright © 2020 NEAR Protocol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
